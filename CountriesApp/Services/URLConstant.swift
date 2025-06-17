230c5338620edf8ffc770d7ed09cd881fd20c386
Enkhtsetseg Unurbayar <eegii.swift@gamil.com>
2025-06-17 14:30:57 -0700

    [Feature] added url constant

---
+++ CountriesApp/Services/URLConstant.swift
@@ -0,0 +1,12 @@
+//
+//  URLConstant.swift
+//  CountriesApp
+//
+//  Created by Enkhtsetseg Unurbayar on 6/17/25.
+//
+
+import Foundation
+
+struct URLConstant {
+    static let contriesURL = "https://gist.githubusercontent.com/peymano-wmt/32dcb892b06648910ddd40406e37fdab/raw/db25946fd77c5873b0303b858e861ce724e0dcd0/countries.json"
+}
