3a9462f076617630855a35e062063e1d94da460a
Enkhtsetseg Unurbayar <eegii.swift@gamil.com>
2025-06-17 13:59:46 -0700

    [Create] mvvm architecture set up

---
+++ CountriesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CountriesApp
+//
+//  Created by Enkhtsetseg Unurbayar on 6/17/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
